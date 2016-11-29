0eec93d3d81d6de57f7ea98e05387107dfe65e73
Pedro Paulo Palazzo <palazzo@abaco-arquitetura.com.br>
2016-11-29 01:35:13 -0200

    Setting up pgsql database connection + scoping tests

---
+++ src/sicg/functional_tests.py
@@ -0,0 +1,9 @@
+'''
+This is the file for prototyping application functionality.
+Comments indicate expected user input and application behavior,
+tested by the code.
+'''
+
+# Django can connect to the database
+
+# User loads home page
--- src/sicg/sicg/settings.py
+++ src/sicg/sicg/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sicg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sicg',
+        'USER': 'sicg',
+        'PASSWORD': 'iphan.2016',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
