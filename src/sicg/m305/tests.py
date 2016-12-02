d5d4163607c910a08a68bc00abe694c5d99781a1
Pedro Paulo Palazzo <palazzo@abaco-arquitetura.com.br>
2016-12-01 23:07:03 -0200

    Preparing project for creating first model

--- src/sicg/m305/tests.py
+++ src/sicg/m305/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from m305.views import index
 
-# Create your tests here.
+class homePageTest(TestCase):
+    def rootResolvesToHome(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
