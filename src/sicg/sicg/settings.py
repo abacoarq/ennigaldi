f7d210b92984b0742f1d4a1c995556b8560423de
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-01-23 17:42:52 -0200

    Registering applications

--- src/sicg/sicg/settings.py
+++ src/sicg/sicg/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'objectinfo',
+    'historicdate',
+    'agent',
+    'place',
+    'countries_plus',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'countries_plus.middleware.AddRequestCountryMiddleware',
 ]
 
 ROOT_URLCONF = 'sicg.urls'
@@ -122,3 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Countries Plus Headers
+# https://github.com/cordery/django-countries-plus
+
+COUNTRIES_PLUS_COUNTRY_HEADER = 'HTTP_CF_COUNTRY'
+COUNTRIES_PLUS_DEFAULT_ISO = 'BR'
