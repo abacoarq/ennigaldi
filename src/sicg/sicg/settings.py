eb3320f7132e3a48f9d6bca82d40d1367dc4ad4f
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-01-23 19:56:12 -0200

    Unregistering Countries_plus middleware
    To avoid TypeError (object takes no parameters).

--- src/sicg/sicg/settings.py
+++ src/sicg/sicg/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'countries_plus.middleware.AddRequestCountryMiddleware',
+#    'countries_plus.middleware.AddRequestCountryMiddleware',
 ]
 
 ROOT_URLCONF = 'sicg.urls'
