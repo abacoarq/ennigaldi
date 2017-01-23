575296cb5ffa9c03d63aa50a10bf8b02f029f91d
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-01-22 22:20:22 -0200

    Creating dedicated apps for Agent and Place

---
+++ src/sicg/agent/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+###########################################################
+# Spectrum 4.0 organization, people, person
+# VRA Core 4   agent
+# Dublin Core  contributor, creator, publisher
+class Agent(models.Model):
+    class Meta:
+        abstract = True
+# /Spectrum 4.0 organization, people, person
+###########################################################
