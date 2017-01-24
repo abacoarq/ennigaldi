7a2441ca4728b3066e5c4e25326eabba39fca67a
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-01-23 23:38:33 -0200

    Fixing positional arg error

--- src/sicg/agent/models.py
+++ src/sicg/agent/models.py
@@ -24,14 +24,14 @@ class Agent(models.Model):
     # Use controlled vocab, can be replaced by nationality
     # in the case of modern agents.
     agent_culture = models.CharField(max_length=63, blank=True)
-    agent_date = models.ManyToManyField(HistoricDate, models.CASCADE, through='AgentDateType')
+    agent_date = models.ManyToManyField(HistoricDate, 'date_for_agent', models.CASCADE, through='AgentDateType')
     # Use this for complex name display or autopopulate from
     # above data using a pre-save hook.
     agent_display = models.CharField(max_length=255)
     # Further identification, if available
     user = models.OneToOneField(User, blank=True)
     orcid = models.CharField(max_length=31, blank=True)
-    # agent_affiliation = models.ManyToManyField('self', models.SET_NULL, through='AgentAffiliation')
+    agent_affiliation = models.ManyToManyField('self', 'employs_agent', models.SET_NULL, through='AgentAffiliation', symmetrical=False)
     # Contact information, if applicable
     email = models.EmailField(blank=True)
     phone_primary = models.CharField(max_length=31, blank=True)
@@ -58,9 +58,9 @@ class AgentDateType(DateType):
     )
     date_of = models.ForeignKey(Agent, models.CASCADE)
 
-# class AgentAffiliation(models.Model):
-#     agent_person = models.ForeignKey(Agent, limit_choices_to={name_type: 'personal'})
-#     agent_affiliation = models.ForeignKey(Agent, limit_choices_to=Q(name_type!='personal'))
-#     agent_role = models.CharField(max_length=255)
+class AgentAffiliation(models.Model):
+     agent_person = models.ForeignKey(Agent, models.CASCADE, related_name='employed_in', limit_choices_to={Agent.name_type: 'personal'})
+     agent_affiliation = models.ForeignKey(Agent, models.PROTECT, related_name='employs', limit_choices_to=Q(Agent.name_type!='personal'))
+     agent_role = models.CharField(max_length=255)
 # /Spectrum 4.0 organization, people, person
 ###########################################################
