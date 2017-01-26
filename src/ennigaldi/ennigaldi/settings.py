f849a319f672069d43779f6e80a3c09678931740
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-01-26 14:38:03 -0200

    Articulating AccessionNumber and ObjectInfo

--- src/ennigaldi/ennigaldi/settings.py
+++ src/ennigaldi/ennigaldi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'place',
     'agent',
     'objectinfo',
+    'reorg',
 ]
 
 MIDDLEWARE = [
--- src/ennigaldi/reorg/models.py
+++ src/ennigaldi/reorg/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 import datetime
 
-class AccessionTable(models.Model):
-    year = models.DateField(auto_now_true)
-    num = models.PositiveSmallIntegerField(unique_for_year="year")
+class AccessionNumber(models.Model):
+    accession_number_display = models.CharField(max_length=15)
+    year = models.DateField(auto_now_add=True)
     retrospective = models.BooleanField(default=False)
+    object_number = models.PositiveSmallIntegerField(unique_for_year="year")
+    part_number = models.PositiveSmallIntegerField(blank=True)
+
+    class Meta:
+        unique_together=('object_number', 'part_number', 'year')
+        ordering = ['-year', '-object_number', 'part_number']
