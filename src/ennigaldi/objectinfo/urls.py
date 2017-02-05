c2b5136bc2569a651e61cdc379ae6c67e5fe041f
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-02-05 11:41:28 -0200

    Adding Views placeholders

--- src/ennigaldi/objectinfo/urls.py
+++ src/ennigaldi/objectinfo/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^work/$', views.object_list)
+    url(r'^$', views.index),
+    url(r'^work/$', views.object_list),
+    url(r'^work/new/$', views.field_form),
+    url(r'^work/', views.m305),
 ]
