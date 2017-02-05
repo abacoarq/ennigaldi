25ca1506c49e9b4298eb06e50d62b81a269d5f82
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-02-05 11:41:28 -0200

    Adding Views placeholders

--- src/ennigaldi/objectinfo/views.py
+++ src/ennigaldi/objectinfo/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Nothing here yet.')
+
+def field_form(request):
+    return HttpResponse('This is where the field survey form will appear.')
+
+def next_entry(request):
+    return HttpResponse('This page shows after saving an entry, offering to add another.')
+
+def object_list(request):
+    return HttpResponse('There will be a list of entered works here.')
+
+def image_form(request):
+    return HttpResponse('A form to enter images, possibly in bulk, will appear here.')
+
+def m305(request):
+    return HttpResponse('SICG M305')
