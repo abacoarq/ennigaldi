7e0e1c57b65ecf9334ee4eca6b9cc0ef2573855c
Pedro P. Palazzo <pedro.palazzo@gmail.com>
2017-02-08 16:02:21 -0200

    Setting up AccessionNumber.generate() test
    Need to revise generate() method and complete test assertions before running tests.

--- src/ennigaldi/reorg/tests.py
+++ src/ennigaldi/reorg/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 from .models import *
+from objectinfo.models import ObjectIdentification, ObjectHierarchy, ObjectName, IsoLanguage
 
 class TestStartBatch(TestCase):
     def setUp(self):
@@ -20,7 +22,16 @@ class TestStartBatch(TestCase):
 
 def TestGenerateRefid(TestCase):
     def setUp(self):
-        pass
+        AccessionBatch.start_batch(retrospective=True, batch_note="Retrospective")
+        ptbr = IsoLanguage.objects.create(iso="pt_BR", language="Portuguese (Brazil)")
+        t1 = ObjectName.objects.create(title="Some object", title_type="descriptive", note="Didn't know what to name it", lang=ptbr, source="My own mind", translation="Ceci n'est pas un titre")
+        o1 = ObjectIdentification.objects.create(snapshot=image, source="source of information", brief_description="Description text.", description_source="citation for description", comments="Some comments", distinguishing_features="This object is peculiar because it is a test object.", work_type="Fiddle", preferred_title=t1)
+        t2 = ObjectName.objects.create(title="Mona Lisa", title_type="creator", note="Uncertain identification of subject", lang=ptbr, source="Wölfflin")
+        o2 = ObjectIdentification.objects.create(snapshot=image, source="Vasari", brief_description="Portrait of Lisa Gherardini", work_type="Painting", preferred_title=t2)
+        p1 = ObjectHierarchy.objects.create(lesser=o1, greater=o2, relation_type='partOf')
+        image.close()
+        n1 = AccessionNumber.generate()
+        n2 = AccessionNumber.generate()
 
     def test_generate_refid(self):
         """
