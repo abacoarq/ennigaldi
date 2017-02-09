36be5cc12bbd66bfe76fc86afbe1c61a2d2d21cc
Pedro P. Palazzo <pedro.palazzo@gmail.com>
2017-02-09 21:39:59 -0200

    Creating unit detail view url

--- src/ennigaldi/storageunit/urls.py
+++ src/ennigaldi/storageunit/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.unit_list, name='unit_list'),
+    url(r'^(?P<unit_id>[0-9]+)/$', views.unit_detail, name='unit_detail'),
     url(r'^add/$', views.field_form, name='field_entry_form'),
 ]
