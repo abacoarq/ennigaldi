9c7ac57cf0ffe51d72340a7022aea4baf9ace8e1
Pedro P. Palazzo <pedro.palazzo@gmail.com>
2017-02-09 21:36:14 -0200

    Create rendered view for list and detail

--- src/ennigaldi/storageunit/views.py
+++ src/ennigaldi/storageunit/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 from .models import Unit
 
 def unit_list(request):
-    return HttpResponse('A list of storage units.')
+    list_of_units = Unit.objects.all()
+    return render(request, 'storageunit/list.html', {'list_of_units': list_of_units})
+
+def unit_detail(request, unit_id):
+    unit_data = get_object_or_404(Unit, pk=unit_id)
+    return render(request, 'storageunit/detail.html', {'unit_data': unit_data})
 
 def field_form(request):
     return HttpResponse('The unit entry form.')
