73407bbf4043780ba56d2854ccb222b3f28fc71f
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-03-05 21:13:49 -0300

    Moving to, as yet not functional, class-based views

--- src/ennigaldi/storageunit/urls.py
+++ src/ennigaldi/storageunit/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.unit_list, name='unit_list'),
+    url(r'^$', views.UnitList.as_view(), name='unit_list'),
     url(r'^(?P<unit_id>[0-9]+)/$', views.unit_detail, name='unit_detail'),
-    url(r'^add/$', views.field_form, name='field_entry_form'),
+    url(r'^add/$', views.FieldForm, name='field_entry_form'),
 ]
--- src/ennigaldi/storageunit/views.py
+++ src/ennigaldi/storageunit/views.py
@@ -1,18 +1,32 @@
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.utils.decorators import method_decorator
 from .models import Unit
 
-class unit_list(ListView):
+class UnitList(ListView):
     model = Unit
     template_name = 'storageunit/unit_list.html' # default
-    context_object_name = unit_list # default
+    # context_object_name = unit_list # default
     paginate_by = 25
     queryset = Unit.objects.all() # default
+
+class top_level_units(ListView):
+    model = Unit
+    template_name = 'storageunit/unit_list.html' # default
+    paginate_by = 25
+    queryset = Unit.objects.filter(parent=None)
 
 def unit_detail(request, unit_id):
     unit_data = get_object_or_404(Unit, pk=unit_id)
     return render(request, 'storageunit/detail.html', {'unit_data': unit_data})
 
-def field_form(request):
-    return HttpResponse('The unit entry form.')
+# @method_decorator(login_required, name='dispatch')
+class FieldForm(forms.ModelForm):
+    class Meta:
+        model = Unit
+        fields = ('acronym', 'name', 'parent', 'note')
