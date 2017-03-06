73f07594311e0f4da4e03e8325b3cb4d1fcbe892
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-03-05 21:13:49 -0300

    Moving to, as yet not functional, class-based views

--- src/ennigaldi/storageunit/models.py
+++ src/ennigaldi/storageunit/models.py
@@ -13,16 +13,16 @@ class Unit(models.Model):
     # e.g. building > wing > room > furniture > shelf
     # or in any other way required by the organization.
     # Root-level locations will have this set to NULL:
-    parent = models.ForeignKey('self', models.CASCADE, null=True)
+    parent = models.ForeignKey('self', models.CASCADE, null=True, help_text="Storage units are arranged in hierarchies, such as:<br />Room > Furniture > Shelf")
     # A code that identifies the location, if any.
-    acronym = models.CharField(max_length=15)
+    acronym = models.CharField(max_length=15, help_text="A code or number that identifies the location, if any. Best practices:<br />- Rooms should be numbered<br />- Shelves should be designated with capital letters starting from the bottom")
     # Keep the name short, follow conventions
-    name = models.CharField(max_length=31, blank=True)
+    name = models.CharField(max_length=31, blank=True, help_text="Sould be clear and short")
     unit_type = models.CharField(max_length=31, choices=unit_types)
     # Spectrum 4.0 Location note
     # VRA Core 4   location > notes
     # Notes on the location or its name (e.g. "so-called", "condemned", etc.)
-    note = models.TextField(blank=True)
+    note = models.TextField(blank=True, help_text="Any required observations on the identification or conditions of this unit")
 
     def __str__(self):
         if self.parent:
