b78c5f707f2dcc0352db15db4066c6213dd2b211
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-03-06 11:53:19 -0300

    Functioning storage Unit module, though still ugly

--- src/ennigaldi/storageunit/models.py
+++ src/ennigaldi/storageunit/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Unit(models.Model):
     unit_types = (
@@ -9,15 +10,18 @@ class Unit(models.Model):
             ('library', 'Library'),
             ('transport', 'Transport'),
             )
+
+    def get_parent(parent_id):
+        pass
     # Locations can be recursive for maximum flexibility,
     # e.g. building > wing > room > furniture > shelf
     # or in any other way required by the organization.
     # Root-level locations will have this set to NULL:
-    parent = models.ForeignKey('self', models.CASCADE, null=True, help_text="Storage units are arranged in hierarchies, such as:<br />Room > Furniture > Shelf")
+    parent = models.ForeignKey('self', models.CASCADE, related_name="children_set", null=True, blank=True, help_text="Storage units are arranged in hierarchies, such as:<br />Room > Furniture > Shelf")
     # A code that identifies the location, if any.
     acronym = models.CharField(max_length=15, help_text="A code or number that identifies the location, if any. Best practices:<br />- Rooms should be numbered<br />- Shelves should be designated with capital letters starting from the bottom")
     # Keep the name short, follow conventions
-    name = models.CharField(max_length=31, blank=True, help_text="Sould be clear and short")
+    name = models.CharField(max_length=31, blank=True, help_text="Use if needed to clarify the acronym only. Sould be clear and short")
     unit_type = models.CharField(max_length=31, choices=unit_types, default='exhibit')
     # Spectrum 4.0 Location note
     # VRA Core 4   location > notes
@@ -32,7 +36,7 @@ class Unit(models.Model):
         return parent_string + self.acronym + ' ' + self.name
 
     def get_absolute_url(self):
-        return reverse('unit-detail', kwargs={'pk': self.pk})
+        return reverse('unit_detail', kwargs={'pk': self.pk})
 
     class Meta:
         unique_together = ('parent', 'acronym')
--- src/ennigaldi/storageunit/views.py
+++ src/ennigaldi/storageunit/views.py
@@ -1,16 +1,17 @@
 from django import forms
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.urls import reverse_lazy
+from django.db import transaction
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
 from .models import Unit
-from .forms import FieldForm
+from .forms import ParentUnitForm, ChildUnitForm, unit_formset
 
 class UnitList(ListView):
     model = Unit
@@ -29,12 +30,59 @@ class UnitDetail(DetailView):
 @method_decorator(login_required, name='dispatch')
 class AddUnit(CreateView):
     model = Unit
-    fields = ['acronym', 'name', 'parent', 'note']
+    form = ParentUnitForm
+    fields = ['type', 'acronym', 'name', 'note']
+
+    def get_context_data(self, **kwargs):
+        data = super(AddUnit, self).get_context_data(**kwargs)
+        if self.request.POST:
+            data['unitchildren'] = unit_formset(self.request.POST)
+        else:
+            data['unitchildren'] = unit_formset()
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        unitchildren = context['unitchildren']
+        with transaction.atomic():
+            self.object = form.save()
+
+            if unitchildren.is_valid():
+                unitchildren.instance = self.object
+                unitchildren.save()
+        return super(AddUnit, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('unit_list')
 
 @method_decorator(login_required, name='dispatch')
 class UpdateUnit(UpdateView):
     model = Unit
-    fields = ['acronym', 'name', 'parent', 'note']
+    form = ParentUnitForm
+    fields = ['type', 'acronym', 'name', 'parent', 'note']
+
+    def get_context_data(self, **kwargs):
+        data = super(UpdateUnit, self).get_context_data(**kwargs)
+        if self.request.POST:
+            data['unitchildren'] = unit_formset(self.request.POST)
+        else:
+            data['unitchildren'] = unit_formset()
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        unitchildren = context['unitchildren']
+        with transaction.atomic():
+            self.object = form.save()
+
+            if unitchildren.is_valid():
+                unitchildren.instance = self.object
+                unitchildren.save()
+        return super(UpdateUnit, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('unit_list')
+
 
 @method_decorator(login_required, name='dispatch')
 class DeleteUnit(DeleteView):
