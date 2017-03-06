f60870ced7fbc9bb6bb85f48e713568a463fd09c
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-03-05 21:13:28 -0300

    Enabling login and logout urls

--- src/ennigaldi/ennigaldi/urls.py
+++ src/ennigaldi/ennigaldi/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^work/', include('objectinfo.urls')),
     url(r'^unit/', include('storageunit.urls')),
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.logout, name='logout'),
 ]
