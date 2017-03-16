abd337fd3ddbd9bf7eea37f32099d7bd94fc0136
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-03-16 05:03:21 -0300

    Formatting M305 display

--- src/ennigaldi/objectinfo/urls.py
+++ src/ennigaldi/objectinfo/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^add/(?P<work_id>[0-9]+)/description/', views.DescriptionEntry.as_view(), name='description_entry'),
     url(r'^add/$', views.title_entry, name='titleentry_form'),
     url('^(?P<pk>[0-9]+)/$', views.ObjectDetail.as_view(), name='objectregister_detail'),
-    url(r'^sicg/(?P<pk>[0-9]+)/', views.ObjectDetail.as_view(template_name="sicg_m305.html"), name='sicg_m305'),
+    url(r'^sicg/(?P<pk>[0-9]+)/', views.ObjectDetail.as_view(template_name="objectinfo/sicg_m305.html"), name='sicg_m305'),
     url(r'^xml/(?P<pk>[0-9]+)/', views.xml, name='vra_core_xml'),
     url(r'^yaml/(?P<pk>[0-9]+)/', views.yaml, name='yaml'),
     url(r'^', views.ObjectList.as_view(), name='object_list'),
