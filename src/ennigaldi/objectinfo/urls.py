25a37d771a27a81dd5c74c457baf329856efddde
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-03-15 23:14:40 -0300

    Fixed object display in list

--- src/ennigaldi/objectinfo/urls.py
+++ src/ennigaldi/objectinfo/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     url(r'^add/{?P<work_id>[0-9]+/production/', views.ProductionEntry.as_view(), name='production_entry'),
     url(r'^add/(?P<work_id>[0-9]+)/description/', views.DescriptionEntry.as_view(), name='description_entry'),
     url(r'^add/$', views.title_entry, name='titleentry_form'),
-    url(r'^sicg/(?P<work_id>[0-9]+)/', views.ObjectDetail.as_view(template_name="sicg_m305.html"), name='sicg_m305'),
-    url(r'^xml/(?P<work_id>[0-9]+)/', views.xml, name='vra_core_xml'),
-    url(r'^yaml/(?P<work_id>[0-9]+)/', views.yaml, name='yaml'),
+    url('^(?P<pk>[0-9]+)/$', views.ObjectDetail.as_view(), name='objectregister_detail'),
+    url(r'^sicg/(?P<pk>[0-9]+)/', views.ObjectDetail.as_view(template_name="sicg_m305.html"), name='sicg_m305'),
+    url(r'^xml/(?P<pk>[0-9]+)/', views.xml, name='vra_core_xml'),
+    url(r'^yaml/(?P<pk>[0-9]+)/', views.yaml, name='yaml'),
     url(r'^', views.ObjectList.as_view(), name='object_list'),
 ]
