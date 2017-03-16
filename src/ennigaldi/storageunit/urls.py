fa493e55ef9eb7f3174dc052d0d78ceab6f59a17
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-03-15 22:58:19 -0300

    Fixing behavior of urls

--- src/ennigaldi/storageunit/urls.py
+++ src/ennigaldi/storageunit/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.TopLevelUnits.as_view(), name='unit_list_top'),
     url(r'^all/$', views.UnitList.as_view(), name='unit_list'),
     url(r'^(?P<pk>[0-9]+)/$', views.UnitDetail.as_view(), name='unit_detail'),
     url(r'^(?P<pk>[0-9]+)/edit/$', views.UpdateUnit.as_view(), name='update_unit'),
     url(r'^(?P<pk>[0-9]+)/delete/$', views.DeleteUnit.as_view(), name='delete_unit'),
     url(r'^add/$', views.AddUnit.as_view(), name='field_entry_form'),
+    url(r'^', views.TopLevelUnits.as_view(), name='unit_list_top'),
 ]
