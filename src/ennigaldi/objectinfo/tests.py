1dc59d55331a27257468078e99970e41d6249dfd
PPP on ATX <palazzo@abaco-arquitetura.com.br>
2017-06-24 23:49:58 -0300

    Trying to write test for dimensions

--- src/ennigaldi/objectinfo/tests.py
+++ src/ennigaldi/objectinfo/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
-from .models import ObjectRegister, ObjectName, ObjectUnit, Hierarchy, Production, AgentRole, TechniqueType, ObjectPlaceType, IsoLanguage
+from .models import ObjectRegister, ObjectName, ObjectUnit, Hierarchy, Production, Dimension, AgentRole, TechniqueType, ObjectPlaceType, IsoLanguage
 from storageunit.models import Unit
 from historicdate.models import HistoricDate, DateType
 from agent.models import Agent, AgentDateType, AgentAffiliation
@@ -18,16 +18,31 @@ class TestObjectRegister(TestCase):
         # http://stackoverflow.com/questions/26298821/django-testing-model-with-imagefield
         # Image downloaded from placeholder.it
         image = SimpleUploadedFile(name='test_image.jpg', content=open('../sample/350x150.png', 'rb').read(), content_type='image/jpeg')
+        # Create a required language for inscriptions.
         ptbr = IsoLanguage.objects.create(iso="pt_BR", language="Portuguese (Brazil)")
+        ptbr.save()
+        # Object 1
         t1 = ObjectName.objects.create(title="Some object", title_type="descriptive", note="Didn't know what to name it", lang=ptbr, source="My own mind", translation="Ceci n'est pas un titre")
+        t1.save()
         o1 = ObjectRegister.objects.create(snapshot=image, source="source of information", brief_description="Description text.", description_source="citation for description", comments="Some comments", distinguishing_features="This object is peculiar because it is a test object.", work_type="Fiddle", preferred_title=t1)
+        o1.save()
+        # Object 2
         t2 = ObjectName.objects.create(title="Mona Lisa", title_type="creator", note="Uncertain identification of subject", lang=ptbr, source="Wölfflin")
+        t2.save()
         o2 = ObjectRegister.objects.create(snapshot=image, source="Vasari", brief_description="Portrait of Lisa Gherardini", work_type="Painting", preferred_title=t2)
+        o2.save()
+        # Object 1 is part of object 2
         p1 = Hierarchy.objects.create(lesser=o1, greater=o2, relation_type='partOf')
+        p1.save()
+        # Get rid of image cache
         image.close()
+        # Production information for Object 1
         d1 = HistoricDate.objects.create(display="1503–1506, possibly 1517", earliest="1503", earliest_accuracy=False, latest="1506", latest_accuracy=True)
+        d1.save()
         a1 = Agent.objects.create(display="Leonardo da Vinci (Italian, 1452–1519)", name="Leonardo da Vinci", name_type="personal", culture="Italian Renaissance")
+        a1.save()
         k1 = Production.objects.create(date=d1, note="Attested in Leonardo's workshop in France.")
+        k1.save()
 
     def test_create_work(self):
         """
@@ -40,6 +55,9 @@ class TestObjectRegister(TestCase):
         self.assertEqual(o1.preferred_title.source, "My own mind")
 
     def test_storage_unit(self):
+        """
+        Check that object has been assigned a storage unit.
+        """
         o1 = ObjectRegister.objects.get(brief_description__contains="Description")
         r01f02 = Unit.objects.create(acronym="F02", name="Display case with shelves")
         r01f02a = Unit.objects.create(parent=r01f02, acronym="A", name="Shelf")
@@ -69,3 +87,16 @@ class TestObjectRegister(TestCase):
         o2.production=k1
         o2.save()
         self.assertTrue("Attested" in o2.production.note)
+
+    def test_dimensions(self):
+        """
+        Check that dimension information can be recorded
+        and retrieved for an object.
+        """
+        o2 = ObjectRegister.objects.get(preferred_title__title__contains="Mona")
+        dim1 = Dimension.objects.create(work=o2, dimension_part='Canvas', dimension_type='width', dimension_value=530)
+        dim1.save()
+        dim2 = Dimension.objects.create(work=o2, dimension_part='Canvas', dimension_type='height', dimension_value=770, dimension_value_qualifier=True)
+        dim2.save()
+        print(dim2)
+        # self.assertEqual(o2.measurements.height.dimension_value, 770)
